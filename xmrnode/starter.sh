73b585d497a863a802d0b8193e6911acfb110d5a
sxtq <admin@qubitmail.com>
2021-06-16 01:49:57 +0000

    Update starter.sh

--- xmrnode/starter.sh
+++ xmrnode/starter.sh
@@ -117,7 +117,7 @@ pid_count=$(pgrep starter.sh | wc -l)
 if [ "$pid_count" -gt "2" ]; then
   print "Starter script is already running exiting" yellow
   exit 1
+else
+  starter
+  checkupdate
 fi
-
-starter
-checkupdate
